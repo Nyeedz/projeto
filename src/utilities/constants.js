6c4d475906ca7895eaea6d309e44e2654a099fe6
Nyeedz <chr.alexfb@gmail.com>
2019-06-08 00:14:48 -0300

    funcionario id

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,6 +1,6 @@
 // export const url = 'http://191.252.59.98:7100';
 export const url = 'http://localhost:1337';
-export const funcionarioId = '5cfb202caea8cf2d544d8875';
+export const funcionarioId = '5cfb1fcdaea8cf2d544d87de';
 
 export const CODE_NENHUMA = 0;
 export const CODE_VISUALIZAR = 1;
